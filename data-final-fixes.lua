7710bf9e6fd290f42e06632d1c9deffc32546ea5
Florian Gebhardt <me@fgardt.dev>
2025-08-10 16:19:18 +0200

    feat: updated to 2.0

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,68 +1,65 @@
+---@type table<data.ItemID, true>
 local science_packs = {}
-for _, tech in pairs(data.raw["technology"]) do
-    local hidden = tech.hidden
-    local unit = tech.unit
+---@type table<data.ItemID, data.TechnologyID>
+local tool_2_unlocking_tech = {}
 
-    if tech.normal then
-        hidden = tech.normal.hidden
-        unit = tech.normal.unit
-    elseif tech.expensive then
-        hidden = tech.expensive.hidden
-        unit = tech.expensive.unit
+for _, tech in pairs(data.raw["technology"]) do
+    if tech.hidden then goto continue end
+
+    local effects = tech.effects
+    if not effects then goto skip_effects end
+
+    do
+        ---@type table<data.ItemID, true>
+        local unlocks_tool = {}
+        for _, effect in pairs(effects) do
+            if effect.type ~= "unlock-recipe" then goto continue end
+
+            local recipe = data.raw["recipe"][effect.recipe] or {}
+            local results = recipe.results or {}
+
+            for _, result in pairs(results) do
+                if result.type ~= "item" then goto continue end
+
+                local res_name = result.name
+                if data.raw["tool"][res_name] then
+                    unlocks_tool[res_name] = true
+                end
+
+                ::continue::
+            end
+
+            ::continue::
+        end
+
+        -- if the tech only unlocks a single tool we can assume that the
+        -- technology icon is a high resolution version of the tool icon
+        -- and can use it for the armor instead of the low resolution item icon
+        if table_size(unlocks_tool) == 1 then
+            local uname, _ = next(unlocks_tool)
+            ---@cast uname -?
+
+            local existing_tech = tool_2_unlocking_tech[uname]
+            if not existing_tech then
+                tool_2_unlocking_tech[uname] = tech.name
+            elseif uname == tech.name then
+                tool_2_unlocking_tech[uname] = tech.name
+            end
+        end
     end
 
-    if hidden or not unit then goto continue end
+    ::skip_effects::
+
+    local unit = tech.unit
+    if not unit then goto continue end
 
     for _, pack in pairs(unit.ingredients) do
-        local name = pack.name or pack[1]
+        local name = pack[1]
 
         -- only convert tool based science packs
         -- will not touch armors / repair packs
-        if data.raw["tool"][name] and not science_packs[name] then
-            science_packs[name] = {}
-        end
-
-        local unlocks = {}
-        for _, effect in pairs(tech.effects or {}) do
-            if effect.type == "unlock-recipe" then
-                local recipe = data.raw["recipe"][effect.recipe] or {}
-                local results = recipe.results or { { recipe.result } }
-                if recipe.normal then
-                    results = recipe.normal.results or { { recipe.normal.result } }
-                elseif recipe.expensive then
-                    results = recipe.expensive.results or { { recipe.expensive.result } }
-                end
-
-                for _, result in pairs(results) do
-                    local res_name = result.name or result[1]
-
-                    if data.raw["tool"][res_name] then
-                        unlocks[res_name] = true
-                    end
-
-                    for itype, _ in pairs(defines.prototypes.item) do
-                        ---@type data.ItemPrototype
-                        local item = data.raw[itype][res_name]
-                        if item then
-                            local rocket_result = item.rocket_launch_products or { item.rocket_launch_product }
-                            for _, r in pairs(rocket_result) do
-                                local r_name = r.name or r[1]
-                                if data.raw["tool"][r_name] then
-                                    unlocks[r_name] = true
-                                end
-                            end
-                        end
-                    end
-                end
-            end
-        end
-
-        if table_size(unlocks) == 1 then
-            local uname, _ = next(unlocks)
-            local scp = science_packs[uname] or {}
-            if scp.tech == nil then
-                science_packs[uname] = { tech = tech.name }
-            end
+        if data.raw["tool"][name] then
+            science_packs[name] = true
         end
     end
 
@@ -116,13 +113,14 @@ for name, info in pairs(science_packs) do
     local layers = icon2layers(pack, scale)
     local scale = 0.75
 
-    if name == "automation-science-pack" and not info.tech then
+    local tech = tool_2_unlocking_tech[name]
+    if name == "automation-science-pack" and not tech then
         layers = icon2layers({
             icon = "__base__/graphics/technology/automation-science-pack.png",
             icon_size = 256
         }, scale)
-    elseif info.tech then
-        layers = icon2layers(data.raw["technology"][info.tech], scale)
+    elseif tech then
+        layers = icon2layers(data.raw["technology"][tech], scale)
     end
 
     ---@type data.RotatedAnimation
@@ -159,13 +157,14 @@ end
 
 data:extend(armors)
 
-if table_size(data.raw["tool"]) == 0 and table_size(armors) > 0 then
-    local dummy = table.deepcopy(armors[1])
-    dummy.type = "tool"
-    dummy.name = "SPA-dummy-tool"
-    dummy.flags = { "hidden" }
-    data:extend({ dummy })
-end
+-- if table_size(data.raw["tool"]) == 0 and table_size(armors) > 0 then
+--     local dummy = table.deepcopy(armors[1])
+--     dummy.type = "tool"
+--     dummy.name = "SPA-dummy-tool"
+--     dummy.hidden = true
+--     dummy.hidden_in_factoriopedia = true
+--     data:extend({ dummy })
+-- end
 
 for _, char in pairs(data.raw["character"]) do
     for _, anim in pairs(anims) do
